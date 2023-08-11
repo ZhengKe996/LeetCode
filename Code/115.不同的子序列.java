6a5f35630fb6c6f1589992266730af11cbfd861e
zhengke <zhengke996@163.com>
2023-08-11 18:14:43 +0800

    2023年08月11日18:14:42

--- Code/115.不同的子序列.java
+++ Code/115.不同的子序列.java
@@ -7,8 +7,23 @@
 // @lc code=start
 class Solution {
   public int numDistinct(String s, String t) {
+    int n = s.length(), m = t.length();
+    s = " " + s;
+    t = " " + t;
 
-    return 0;
+    int[][] f = new int[n + 1][m + 1];
+    for (int i = 0; i < n; i++)
+      f[i][0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= m; j++) {
+        f[i][j] = f[i - 1][j];
+        if (s.charAt(i) == t.charAt(j) && f[i][j] <= 2147483647 - f[i - 1][j - 1]) {
+          f[i][j] += f[i - 1][j - 1];
+        }
+      }
+    }
+    return f[n][m];
   }
 }
 // @lc code=end
